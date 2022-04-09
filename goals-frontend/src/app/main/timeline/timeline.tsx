1a230fd4a91aedbcfc954e6b02a25ee8fe9f0fc8
Jessalyn Wang <jess4lynwang@gmail.com>
2022-04-09 16:12:47 -0700

    added goal interface

--- goals-frontend/src/app/main/timeline/timeline.tsx
+++ goals-frontend/src/app/main/timeline/timeline.tsx
@@ -2,12 +2,94 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import './timeline.css';
+import TimelineLine from './timeline-line/timeline-line';
+import { useParams } from 'react-router';
 
 function Timeline() {
+  let { userId } = useParams();
+
+  console.log("found id", userId);
   
+  const goalData = [
+    {
+      '_id': 'foo',
+      'title': 'SD Hacks',
+      'description': 'Submit a cool project for sd hacks 2022! We want to make Goals, an app for users to create, manage and follow goals. The stack is React, Flask, and MongoDB',
+      'creator': 'Udo',
+      'start_date': new Date(),
+      'end_date': new Date(),
+
+      'subgoals': [],
+      '_parentId': "",
+      'depth': 0,
+      'tags': [],
+
+      'followers': [],
+      'follower_count': 0,
+
+      'inspired_goals': [],
+    },
+
+    {
+      '_id': 'foo',
+      'title': 'SD Hacks',
+      'description': 'Submit a cool project for sd hacks 2022! We want to make Goals, an app for users to create, manage and follow goals. The stack is React, Flask, and MongoDB',
+      'creator': 'Udo',
+      'start_date': new Date(),
+      'end_date': new Date(),
+
+      'subgoals': [],
+      '_parentId': "",
+      'depth': 0,
+      'tags': [],
+
+      'followers': [],
+      'follower_count': 0,
+
+      'inspired_goals': [],
+    },
+
+    {
+      '_id': 'foo',
+      'title': 'SD Hacks',
+      'description': 'Submit a cool project for sd hacks 2022! We want to make Goals, an app for users to create, manage and follow goals. The stack is React, Flask, and MongoDB',
+      'creator': 'Udo',
+      'start_date': new Date(),
+      'end_date': new Date(),
+
+      'subgoals': [],
+      '_parentId': "",
+      'tags': [],
+
+      'followers': [],
+      'follower_count': 0,
+
+      'inspired_goals': [],
+    },
+
+    {
+      '_id': 'foo',
+      'title': 'SD Hacks',
+      'description': 'Submit a cool project for sd hacks 2022! We want to make Goals, an app for users to create, manage and follow goals. The stack is React, Flask, and MongoDB',
+      'creator': 'Udo',
+      'start_date': new Date(),
+      'end_date': new Date(),
+
+      'subgoals': [],
+      '_parentId': "",
+      'tags': [],
+
+      'followers': [],
+      'follower_count': 0,
+
+      'inspired_goals': [],
+    },
+  ]
+
   return (
     <div className='timeline'>
       <h4>This is the timeline</h4>
+      <TimelineLine></TimelineLine>
     </div>
   )
 }
---
+++ goals-frontend/src/app/shared/goal-card/goal-card.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+import './goal-card.css';
+
+function GoalCard() {
+  
+  return (
+    <div className='goalcard'>
+      <h4>This is the GoalCard</h4>
+    </div>
+  )
+}
+
+export default GoalCard;
