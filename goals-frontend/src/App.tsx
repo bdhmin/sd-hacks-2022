2f5cfd7426f322a9060dda1ec002a5910f1d425c
Jessalyn Wang <jess4lynwang@gmail.com>
2022-04-09 00:27:23 -0700

    done basic setup for repo

--- goals-frontend/src/App.tsx
+++ goals-frontend/src/App.tsx
@@ -1,23 +1,40 @@
 import React from 'react';
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  function getDataTemplate() {
+    axios.get('/users')
+      .then((response: any) => {
+        console.log(response);
+      })
+      .catch((_: any) => {
+        console.log('ERROR');
+      })
+  }
+
+  function postDataTemplate() {
+    axios.post('/users', {
+      firstName: 'John',
+      lastName: 'Doe',
+      emailId: 'email@email.com'
+    })
+    .then((_: any) => {
+      console.log("success posting")
+    })
+    .catch((_: any) => {
+      console.log("error posting")
+    })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => postDataTemplate()}>click to post</button>
+        <button onClick={() => getDataTemplate()}>click to get</button>
       </header>
     </div>
   );
