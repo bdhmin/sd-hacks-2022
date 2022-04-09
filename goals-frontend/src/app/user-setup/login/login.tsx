ab9d2d2bbd5b0fecebab04e8c8126667e7409e27
Bryan Min <43192371+bdmin00@users.noreply.github.com>
2022-04-09 14:19:17 -0700

    User Creation

--- goals-frontend/src/app/user-setup/login/login.tsx
+++ goals-frontend/src/app/user-setup/login/login.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import './login.css';
 
 function Login() {
+
+  const [username, setUsername] = useState(0);
+  const [password, setpassword] = useState(0);
+
+  function loginUser() {
+    console.log('Got username', username);
+    console.log('Got password', password);
+  }
   
   return (
     <div className='login'>
