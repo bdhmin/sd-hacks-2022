80f614245276528646c890d299d46422e4f29a6c
Jessalyn Wang <jess4lynwang@gmail.com>
2022-04-10 02:58:13 -0700

    explore frontend changes

--- goals-frontend/src/app/main/explore/explore.tsx
+++ goals-frontend/src/app/main/explore/explore.tsx
@@ -6,6 +6,7 @@ import { Goal } from '../../+types/goal';
 import { Tag } from '../../+types/tag';
 import { Autocomplete, TextField } from '@mui/material';
 import GoalCard from '../../shared/goal-card/goal-card';
+import { findRenderedDOMComponentWithTag } from 'react-dom/test-utils';
 
 function Explore() {
     const userId = localStorage.getItem('currentUserId');
@@ -13,110 +14,6 @@ function Explore() {
     const [goals, setGoals] = useState<Goal[]>([]);
     const [tags, setTags] = useState<Tag[]>([]);
     const [filteredTags, setFilteredTags] = useState<Tag[]>([]);
-
-    const tagData = ['education', 'hackathon', 'computer science', 'learning', 'homework', 'school']
-
-    const goalData = [
-        {
-          '_id': 'foo',
-          'title': 'SD Hacks',
-          'description': 'Submit a cool project for sd hacks 2022! We want to make Goals, an app for users to create, manage and follow goals. The stack is React, Flask, and MongoDB',
-          '_creatorId': 'Udo',
-          'start_date': new Date(2022, 11, 17),
-          'end_date': new Date(2022, 11, 17),
-    
-          'subgoals': [],
-          '_parentId': "",
-          'depth': 0,
-          'tags': ['education', 'hackathon', 'computer science', 'learning'],
-    
-          'followers': [],
-          'follower_count': 0,
-    
-          'inspired_goals': [],
-          'inspired_by': null,
-        },
-    
-        {
-          '_id': 'bar',
-          'title': 'Finish Homework',
-          'description': 'Needa do that homework',
-          '_creatorId': 'Udo',
-          'start_date': new Date(2022, 11, 18),
-          'end_date': new Date(2022, 11, 18),
-    
-          'subgoals': [],
-          '_parentId': "",
-          'depth': 0,
-          'tags': ['homework', 'school', 'learning'],
-    
-          'followers': [],
-          'follower_count': 4,
-    
-          'inspired_goals': [],
-          'inspired_by': null,
-        },
-    
-        {
-          '_id': 'foo2',
-          'title': 'Not Subgoal 2',
-          'description': 'This is the second subgoal for sd hacks 2022',
-          '_creatorId': 'Udo',
-          'start_date': new Date(2021, 10, 4),
-          'end_date': new Date(2021, 10, 4),
-    
-          'subgoals': [],
-          '_parentId': "",
-          'depth': 0,
-          'tags': [],
-    
-          'followers': [],
-          'follower_count': 0,
-    
-          'inspired_goals': [],
-          'inspired_by': null,
-        },
-    
-        {
-          '_id': 'buz',
-          'title': 'Old Goal',
-          'description': 'This is goal has passed',
-          '_creatorId': 'Udo',
-          'start_date': new Date(2021, 10, 3),
-          'end_date': new Date(2021, 10, 3),
-    
-          'subgoals': [],
-          '_parentId': "",
-          'depth': 0,
-          'tags': [],
-    
-          'followers': [],
-          'follower_count': 0,
-    
-          'inspired_goals': [],
-          'inspired_by': null,
-        },
-    
-        {
-          '_id': 'foo1',
-          'title': 'Not Subgoal 1',
-          'description': 'This is the first subgoal for the sd hacks goal',
-          '_creatorId': 'Udo',
-          'start_date': new Date(2022, 10, 2),
-          'end_date': new Date(2022, 10, 2),
-    
-          'subgoals': [],
-          '_parentId': "",
-          'depth': 0,
-          'tags': [],
-    
-          'followers': [],
-          'follower_count': 0,
-    
-          'inspired_goals': [],
-          'inspired_by': null,
-        },
-      ]
 
     useEffect(() => {
       // get all goals where creator is not equal to current user
@@ -134,6 +31,14 @@ function Explore() {
         console.log("ERROR")
       })
 
+      axios.get('/api/tags/')
+      .then((result: any) => {
+        console.log("result", result)
+        setTags(result['data'])
+      })
+      .catch((_: any) => {
+        console.log("ERROR getting tags")
+      })
     }, [])
     
     // if the user is filtering by tags, filter goals by tag
@@ -141,33 +46,58 @@ function Explore() {
       if (filteredTags.length === 0) {
         return goals
       } else {
-        return []
+        return goals.filter((goal) => {
+          let flag = false;
+          goal.tags.forEach((tag: Tag) => {
+            if (findTag(tag)) {
+              flag = true;
+            }
+          })
+
+          return flag;
+        })
+
+        // return []
       }
+    }
+
+    function findTag(t: Tag) {
+      let flag = false;
+      filteredTags.forEach((tag: Tag) => {
+        if (tag.text === t.text) {
+          flag = true;
+        }
+      })
+
+      return flag
     }
 
     return (
       <div className="explore">
-        <Autocomplete
-          multiple
-          id="tags-outlined"
-          options={tags}
-          getOptionLabel={(option) => option.text}
-          filterSelectedOptions
-          renderInput={(params) => (
-            <TextField
-              {...params}
-              label="FilterGoals"
-              placeholder="Labels"
-            />
-          )}
-          onChange={(_event, newTags) => {
-            setFilteredTags(newTags);
-          }}
-        />
-        <div>
+        <div className="explore-head">Explore Goals</div>
+        <div className="explore-auto">
+          <Autocomplete
+            multiple
+            id="tags-outlined"
+            options={tags}
+            getOptionLabel={(option) => option.text}
+            filterSelectedOptions
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="FilterGoals"
+                placeholder="Labels"
+              />
+            )}
+            onChange={(_event, newTags) => {
+              setFilteredTags(newTags);
+            }}
+          />
+        </div>
+        <div className='cards'>
           {
             filteredGoals().map((goal: Goal, idx: number) => {
-              return <GoalCard goal={goal} key={idx}></GoalCard>
+              return <div className="card-area" key={idx}><GoalCard goal={goal}></GoalCard></div>
             })
           }
         </div>
